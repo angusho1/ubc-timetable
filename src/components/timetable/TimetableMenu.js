33ba0ef982eeb63cba3ce1bc9517dca9161e7a74
angusho1 <angusho.77@gmail.com>
2021-03-02 22:23:14 -0800

    Added styling to timetable menu

--- src/components/timetable/TimetableMenu.js
+++ src/components/timetable/TimetableMenu.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 export class TimetableMenu extends Component {
+    determineBtnClasses(tableKey) {
+        let className = "timetable-menu-btn";
+        if (this.props.currentTableKey === tableKey) {
+            className = `${className} active`;
+        }
+        return className;
+    }
+
     renderTimetableButtons() {
         return this.props.timetables.map(table => {
             const tableKey = table.tableKey;
             const term = table.term;
             return (
-                <button className="timetable-menu-btn" 
+                <button className={this.determineBtnClasses(tableKey)}
                         key={tableKey}
                         onClick={(e) => this.switchTable(tableKey)}>
                         {term}
