28b168b044f8b85c845604855e7ee5ef4bb8fcec
angusho1 <angusho.77@gmail.com>
2021-04-19 21:04:23 -0700

    Made small tweaks to form submission logic

--- src/components/search/SearchForm.js
+++ src/components/search/SearchForm.js
@@ -7,7 +7,6 @@ import { Formik, Form } from 'formik';
 
 const DEPT_REGEX = /^\s*[a-z]{2,4}\s*$/i;
 const COURSE_SECTION_REGEX = /^\s*[a-z0-9]{3,4}\s*$/i;
-const EMPTY_REGEX = /^\s*$/;
 
 function SearchForm(props) {
 
@@ -33,13 +32,11 @@ function SearchForm(props) {
     }
 
     const initSearch = (dept, course, section) => {
-        const courseEmpty = EMPTY_REGEX.test(course);
-        const sectionEmpty = EMPTY_REGEX.test(section);
         const session = { year: 2020, season: 'W' } // TODO: Remove hardcoded session
 
-        if (!sectionEmpty) {
+        if (section.length > 0) {
             props.searchSection({ dept, course, section, session });
-        } else if (!courseEmpty) {
+        } else if (course.length > 0) {
             props.searchCourse({ dept, course, session });
         } else {
             props.searchDept({ dept, session });
--- src/components/search/SearchInput.js
+++ src/components/search/SearchInput.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useField } from 'formik';
 
 function SearchInput({ label, ...props }) {
-    const [field, meta] = useField(props);
-    const [hasFocused, setHasFocused] = useState(false);
+    const [field, meta, helpers] = useField(props);
 
     const determineClasses = () => {
         if (meta.error) {
             return 'error-border';
-        } else if ((meta.touched || hasFocused) && field.value) {
+        } else if (meta.touched && field.value) {
             return 'searched-input';
         }
         return '';
     }
 
     const hidePlaceHolder = (e) => {
-        const clickedInputBox = e.target;
-        clickedInputBox.placeholder = '';
-        setHasFocused(true);
-        // if (props.searched) {
-            // props.clearText();
-        // }
+        e.target.placeholder = '';
+        helpers.setTouched(true);
+        if (meta.touched) {
+            helpers.setValue('');
+        }
     }
 
     const showPlaceholder = (e) => {
