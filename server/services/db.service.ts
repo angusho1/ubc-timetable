550a90c46ab1eabe583fbfb9ce674ffa2ae35fde
angusho1 <angusho.77@gmail.com>
2021-04-25 15:47:59 -0700

    Attempt to fix mongodb connection issue on deployment

--- server/services/db.service.ts
+++ server/services/db.service.ts
@@ -6,7 +6,11 @@ const dbName = process.env.MONGODB_NAME;
 const connection = client.connect();
 
 async function getDb(): Promise<Db> {
-    return await connection.then(client => client.db(dbName));
+    return await connection.then(client => client.db(dbName))
+        .catch(err => {
+            console.log(err);
+            return err;
+        });
 }
 
 const buildingsCollection: Promise<Collection> = getDb().then(db => db.collection('buildings'));
