13d830e0c17a0031a328ac8bfad581689a71cc35
angusho1 <angusho.77@gmail.com>
2021-06-18 22:29:23 -0700

    Did a package update

--- frontend/src/components/result-displays/ResultDisplay.js
+++ frontend/src/components/result-displays/ResultDisplay.js
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ResultDisplays.scss';
 
-export class ResultDisplay extends Component {
-
-    render() {
-        return (
-            <div id="display-box" className="card p-4 bg-light">
-                <div className="card-title h5 fw-bold">{this.props.title}</div>
-                <p>{this.props.subHeading}</p>
-                {this.props.children}
-            </div>
-        )
-    }
+export default function ResultDisplay(props) {
+    return (
+        <div id="display-box" className="card p-4 bg-light">
+            <div className="card-title h5 fw-bold">{props.title}</div>
+            <p>{props.subHeading}</p>
+            {props.children}
+        </div>
+    );
 }
-
-export default ResultDisplay;
