9bd48c317251387d07badfd6c23bd2d3e199c389
angusho1 <angusho.77@gmail.com>
2021-07-01 23:09:08 -0700

    Added clickable buttons for pre-reqs in course result displays

--- frontend/src/components/result-displays/CourseResultDisplay.js
+++ frontend/src/components/result-displays/CourseResultDisplay.js
@@ -2,12 +2,15 @@ import React, { useContext } from 'react';
 import { connect } from 'react-redux';
 import ResultDisplay from './ResultDisplay';
 import ResultDisplayItem from './ResultDisplayItem/ResultDisplayItem';
-import { searchSection } from '../../reducers/searchSlice';
+import { searchSection, searchCourse } from '../../reducers/searchSlice';
 import { getCourseDeptKey, getCourseKey, getCourseTitle, getCourseCredits, getCoursePreReqs, getCourseSections, getSectionKey } from '../../utils/selectors.js';
 import { ScraperContext } from '../AppControl';
+import parse, { attributesToProps } from 'html-react-parser';
 
 export function CourseResultDisplay(props) {
     const scraperType = useContext(ScraperContext);
+    const courseCodeRegex = /[A-Z]{2,4}\s[A-Z0-9]{3,4}/g;
+    const session = { year: 2021, season: 'W' } // TODO: Remove hardcoded session
 
     function getTitle() {
         const courseObj = getCourseObj();
@@ -22,7 +25,7 @@ export function CourseResultDisplay(props) {
         const courseObj = getCourseObj();
         return (<div>
             <div>Credits: <b>{getCourseCredits(courseObj)}</b></div>
-            <div>Pre-Reqs: <b>{getCoursePreReqs(courseObj)}</b></div>
+            <div>Pre-Reqs: {displayPreReqs(courseObj)}</div>
             <div>Sections:</div>
             <div className="list-group result-display-item-container">
                 { renderSections() }
@@ -34,7 +37,6 @@ export function CourseResultDisplay(props) {
         const courseObj = getCourseObj();
         const sections = Object.values(getCourseSections(courseObj));
         return sections.map((section) => {
-            const session = { year: 2021, season: 'W' } // TODO: Remove hardcoded session
             const deptKey = getCourseDeptKey(courseObj);
             const courseKey = getCourseKey(courseObj);
             const sectionKey = getSectionKey(section);
@@ -50,6 +52,34 @@ export function CourseResultDisplay(props) {
         return props.courseObj;
     }
 
+    function displayPreReqs(courseObj) {
+        const prereqs = getCoursePreReqs(courseObj);
+        const courses = prereqs.matchAll(courseCodeRegex);
+        let currCourse = courses.next();
+        let res = prereqs;
+        while (typeof currCourse.value !== 'undefined') {
+            const courseStr = currCourse.value[0];
+            res = res.replace(courseStr, `<span class="badge bg-primary" coursestr="${courseStr}"></span>`);
+            currCourse = courses.next();
+        }
+        const options = {
+            replace: domNode => {
+                if (domNode.attribs && domNode.name === 'span') {
+                    console.log(domNode);
+                    const elemProps = attributesToProps(domNode.attribs);
+                    const courseSplit = elemProps.coursestr.split(' ');
+                    const dept = courseSplit[0];
+                    const course = courseSplit[1];
+                    const courseSearchParams = { dept, course, session, scraperType };
+                    elemProps.onClick = () => props.searchCourse(courseSearchParams);
+                    return <span {...elemProps}>{elemProps.coursestr}</span>;
+                }
+            }
+        }
+
+        return parse(res, options);
+    }
+
     return (
         <ResultDisplay  title={getTitle()}
                         subHeading={getSubHeading()}>
@@ -58,4 +88,4 @@ export function CourseResultDisplay(props) {
     )
 }
 
-export default connect(null, { searchSection })(CourseResultDisplay);
+export default connect(null, { searchSection, searchCourse })(CourseResultDisplay);
